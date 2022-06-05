b8dc7cea42b17ab08b01f5eac8c2e5898b44657c
safintofiqali <safintofiqali@gmail.com>
2022-06-05 17:37:47 +0300

    Adding all the files

---
+++ src/Review.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+const Review = ({ reviews }) => {
+  const checkNumber = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return reviews.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex(checkNumber(index - 1));
+  };
+
+  const randomPerson = () => {
+    let randomIndex = Math.trunc(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  };
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviews[index];
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">Hello</span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          Previous
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          Next
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  );
+};
+
+export default Review;
